406e693ed213b6c9904c9435a9c78d342ea0f247
Camila Scaglioni <camila.pia.scaglioni@gmail.com>
2024-06-28 16:09:29 -0300

    se agrega el service

---
+++ src/service/Api.jsx
@@ -0,0 +1,26 @@
+import Axios from 'axios'
+
+export const API_BASE_URL = 'https://preguntados-api.vercel.app'
+
+const get = (url) => 
+    Axios.get(url)
+        .then((response) => response)
+        .catch((error) => Promise.reject(error.response.data))
+
+const post = (url, body) => 
+    Axios.post(url, body)
+        .then((response) => response)
+        .catch((error) => Promise.reject(error.response.data))
+
+
+const getDifficulty = () => {
+    return get(`${API_BASE_URL}/api/difficulty`)
+}
+
+const getQuestions = (difficulty = 'easy') => {
+    return get(`${API_BASE_URL}/api/questions?difficulty=${difficulty}`);
+};
+
+const postAnswer = (body) => {
+    return post(`${API_BASE_URL}/api/answer`, body)
+}
